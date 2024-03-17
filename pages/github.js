a1ebafa35771cd3483155019aa53e0f67c8504a3
NicolasZapata <35777697+NicolasZapata@users.noreply.github.com>
2024-03-16 22:07:09 -0500

    💄 [Update] Github Section UI.

--- pages/github.js
+++ pages/github.js
@@ -1,41 +1,43 @@
 import Layout from "../components/Layout";
 import _error from "./_error";
 
-const Github = ({user, statusCode}) => {
-  
-  if(statusCode){
-    return <Error />
+const Github = ({ user, statusCode }) => {
+  if (statusCode) {
+    return <Error />;
   }
-  
+
   return (
-    <Layout footer={false} dark>
+    <Layout footer={false}>
       <div className="row">
         <div className="col-md-4 offset-md-4">
-          <div className="card card-body text-center">
+          <div className="card card-body text-light text-center bg-dark" >
             <h1>{user.name}</h1>
             <img src={user.avatar_url} alt="" />
             <p>{user.bio}</p>
-            <a href={user.html_url} target="_blank" className="btn btn-outline-secondary">
+            <a
+              href={user.html_url}
+              target="_blank"
+              className="btn btn-primary"
+            >
               Go to my account!
             </a>
           </div>
         </div>
       </div>
-    </Layout>   
-  )
-  
+    </Layout>
+  );
 };
 
-export async function getServerSideProps(){
-  const res = await fetch('https://api.github.com/users/NicolasZapata')
+export async function getServerSideProps() {
+  const res = await fetch("https://api.github.com/users/NicolasZapata");
   const data = await res.json();
-  const statusCode = res.status > 200 ? res.status : false
+  const statusCode = res.status > 200 ? res.status : false;
   return {
     props: {
       user: data,
-      statusCode
-    }
-  }
+      statusCode,
+    },
+  };
 }
 
 export default Github;
